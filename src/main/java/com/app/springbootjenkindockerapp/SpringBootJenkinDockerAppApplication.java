2e3ea68b3680f5d525fec784068f41b9a72e1874
Akash K K <Akash.KK@ibm.com>
2024-08-14 16:53:19 +0530

    docker-jenkins-integration-sample

---
+++ src/main/java/com/app/springbootjenkindockerapp/SpringBootJenkinDockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.springbootjenkindockerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinDockerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJenkinDockerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/springbootjenkindockerapp/SpringBootJenkinDockerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springbootjenkindockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinDockerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
